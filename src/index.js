dad2edd82e1a0b37a70358227997914e98ada213
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2022-11-28 00:52:58 +0100

    add js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './js/contact-modal';
+import './js/take-order-add';
+import './js/take-order-buy';
---
+++ src/js/contact-modal.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openContactModalBtn: document.querySelector('[data-contact-modal-open]'),
+      closeContactModalBtn: document.querySelector('[data-contact-modal-close]'),
+      ContactModal: document.querySelector('[data-contact-modal]'),
+    };
+  
+    refs.openContactModalBtn.addEventListener('click', toggleContactModal);
+    refs.closeContactModalBtn.addEventListener('click', toggleContactModal);
+  
+    function toggleContactModal() {
+      refs.ContactModal.classList.toggle('is-hidden');
+    }
+  })();
+  
